b9bcf6b14945d6191ca34ea83fcb8a10d9a3fe98
zzolikoff1027 <78103329+zzolikoff1027@users.noreply.github.com>
2021-05-26 15:44:32 -0700

    Add files via upload

---
+++ VR Production/VR Production_actions.js
@@ -0,0 +1,89 @@
+var sc_AnSceneNodeScene_204_0_0_fn = function() {
+
+{
+var _this = this;
+
+//Clicking on the specified symbol instance executes a function.
+_this.arrow6.addEventListener(AnEvent.CLICK, function() {
+
+//Go to the specified scene and play
+anWebgl.gotoScene("Scene 3");
+});
+}
+
+{
+var _this = this;
+
+//Clicking on the specified symbol instance executes a function.
+_this.arrow7.addEventListener(AnEvent.CLICK, function() {
+
+//Go to the specified scene and play
+anWebgl.gotoScene("Scene 5");
+});
+}
+
+};
+var sc_AnSceneNodeScene_203_0_0_fn = function() {
+
+{
+var _this = this;
+
+//Clicking on the specified symbol instance executes a function.
+_this.arrow4.addEventListener(AnEvent.CLICK, function() {
+
+//Go to the specified scene and play
+anWebgl.gotoScene("Scene 2");
+});
+}
+
+{
+var _this = this;
+
+//Clicking on the specified symbol instance executes a function.
+_this.arrow5.addEventListener(AnEvent.CLICK, function() {
+
+//Go to the specified scene and play
+anWebgl.gotoScene("Scene 4");
+});
+}
+
+};
+var sc_AnSceneNodeScene_202_0_1_fn = function() {
+
+{
+var _this = this;
+
+//Clicking on the specified symbol instance executes a function.
+_this.arrow2.addEventListener(AnEvent.CLICK, function() {
+
+//Go to the specified scene and play
+anWebgl.gotoScene("Scene 1");
+});
+}
+
+{
+var _this = this;
+
+//Clicking on the specified symbol instance executes a function.
+_this.arrow3.addEventListener(AnEvent.CLICK, function() {
+
+//Go to the specified scene and play
+anWebgl.gotoScene("Scene 3");
+});
+}
+
+};
+var sc_AnSceneNodeScene_201_0_1_fn = function() {
+
+{
+var _this = this;
+
+//Clicking on the specified symbol instance executes a function.
+_this.arrow1.addEventListener(AnEvent.CLICK, function() {
+
+//Go to the specified scene and play
+anWebgl.gotoScene("Scene 2");
+});
+}
+
+};
